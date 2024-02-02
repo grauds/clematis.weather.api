2bcdfe9424948e71a337c3fa695136edcba4937f
Anton Troshin <11165918+grauds@users.noreply.github.com>
2024-02-02 21:58:14 +0300

    [+] Fixed REST API

--- src/main/java/org/clematis/weather/repository/ImagesController.java
+++ src/main/java/org/clematis/weather/repository/ImagesController.java
@@ -5,22 +5,26 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.java.Log;
 
 /**
  * @author Anton Troshin
  */
-@Component
+@RestController
+@Log
 public class ImagesController {
 
-    private String path = "/home/clematis/weather/images";
+    private String path = "/home/clematis/weather/images/";
 
     @Autowired
     private ImagesRepository imagesRepository;
@@ -29,8 +33,13 @@ public class ImagesController {
     @ResponseBody
     public byte[] getImageWithMediaType(@RequestParam("date")
                                         @DateTimeFormat(pattern = "yyyy-MM-dd")
-                                        Date date) throws IOException {
+                                        Date date) {
         List<String> filesNames = imagesRepository.getImages(date);
-        return !filesNames.isEmpty() ? Files.readAllBytes(Paths.get(path, filesNames.get(0))) : new byte[0];
+        try {
+            return !filesNames.isEmpty() ? Files.readAllBytes(Paths.get(path, filesNames.get(0))) : new byte[0];
+        } catch (IOException e) {
+            log.log(Level.WARNING, e.getMessage());
+            return new byte[0];
+        }
     }
 }
